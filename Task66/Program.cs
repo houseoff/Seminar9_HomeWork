56720a9d02267bcfffcf30a48c670ed4e363e54e
Evgenii Khaustov <hrust656@gmail.com>
2023-02-27 04:07:02 +0300

    init commit

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 66. Задайте значения от M до N. Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N
+int SumOfRange(int m, int n)
+{
+    if (m == n)
+        return n;
+    return m + SumOfRange(m + 1, n);
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма элементов от M до N включительно: {SumOfRange(m, n)}");
---
+++ Task68/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 68. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа M и N.
+int AckermanFunction(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+        return AckermanFunction(m - 1, 1);
+    return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Результат вычисления функции Аккермана: {AckermanFunction(m, n)}");
